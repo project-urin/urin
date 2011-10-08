247aa36165639dbd335f0afbd637c17ce778ee49
Mark Slater <mark.slater@mail.com>
2011-10-08 18:37:46 +0100

    Mark - A little bit of scheme name validation.

--- src/main/net/sourceforge/urin/Scheme.java
+++ src/main/net/sourceforge/urin/Scheme.java
@@ -5,7 +5,16 @@ public final class Scheme {
     private final String value;
 
     public Scheme(final String name) {
+        final char firstChar = name.charAt(0);
+        if (!isAlpha(firstChar)) {
+            throw new IllegalArgumentException("First character must be a-z or A-Z, but was [" + firstChar + "]");
+        }
         value = name.toLowerCase();
+    }
+
+    private static boolean isAlpha(final char testChar) {
+        return (testChar >= 'a' && testChar <= 'z')
+                || (testChar >= 'A' && testChar <= 'Z');
     }
 
     public String value() {
--- src/test/net/sourceforge/urin/SchemeTest.java
+++ src/test/net/sourceforge/urin/SchemeTest.java
@@ -4,10 +4,27 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class SchemeTest {
+    @Test
+    public void lowerCaseSchemesAreUnmolested() throws Exception {
+        assertThat(new Scheme("http").value(), equalTo("http"));
+    }
+
     @Test
     public void schemesAreLowerCased() throws Exception {
         assertThat(new Scheme("HTTP").value(), equalTo("http"));
     }
+
+    @Test
+    public void rejectsInvalidSchemeNameFirstChar() throws Exception {
+        try {
+            new Scheme(".");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [.]"));
+        }
+    }
+
 }
