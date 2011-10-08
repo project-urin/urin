827edaf0ba7451a117fceb4e5cd1d1b9c7516012
Mark Slater <mark.slater@mail.com>
2011-10-08 10:42:28 +0100

    Mark - Make first test pass.

---
+++ src/main/net/sourceforge/urin/Scheme.java
@@ -0,0 +1,6 @@
+package net.sourceforge.urin;
+
+public final class Scheme {
+    public Scheme(final String name) {
+    }
+}
--- src/test/net/sourceforge/urin/SchemeTest.java
+++ src/test/net/sourceforge/urin/SchemeTest.java
@@ -1,4 +1,10 @@
 package net.sourceforge.urin;
 
+import org.junit.Test;
+
 public class SchemeTest {
+    @Test
+    public void canCreateAScheme() throws Exception {
+        new Scheme("http");
+    }
 }
