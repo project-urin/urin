0189aebef412c0270416c798b833695cd82a6776
Mark Slater <mark.slater@mail.com>
2011-10-08 18:42:47 +0100

    Mark - Beef up first scheme character tests.

--- src/test/net/sourceforge/urin/SchemeTest.java
+++ src/test/net/sourceforge/urin/SchemeTest.java
@@ -20,11 +20,61 @@ public class SchemeTest {
     @Test
     public void rejectsInvalidSchemeNameFirstChar() throws Exception {
         try {
+            new Scheme("+");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [+]"));
+        }
+        try {
+            new Scheme("-");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [-]"));
+        }
+        try {
             new Scheme(".");
             fail("Expected an IllegalArgumentException to be thrown");
         } catch (IllegalArgumentException e) {
             assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [.]"));
         }
+        try {
+            new Scheme("4");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [4]"));
+        }
+        try {
+            new Scheme("@");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [@]"));
+        }
+        try {
+            new Scheme("[");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [[]"));
+        }
+        try {
+            new Scheme("`");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [`]"));
+        }
+        try {
+            new Scheme("{");
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), equalTo("First character must be a-z or A-Z, but was [{]"));
+        }
+    }
+
+    @Test
+    public void acceptsTheFullRangeOfValidFirstCharacters() throws Exception {
+        new Scheme("a");
+        new Scheme("z");
+        new Scheme("A");
+        new Scheme("Z");
     }
 
 }
