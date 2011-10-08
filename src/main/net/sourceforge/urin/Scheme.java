fcfad0e42a55ac0903080812bf1a609120b5927b
Mark Slater <mark.slater@mail.com>
2011-10-08 18:02:45 +0100

    Mark - Lowercase scheme name.

--- src/main/net/sourceforge/urin/Scheme.java
+++ src/main/net/sourceforge/urin/Scheme.java
@@ -1,6 +1,14 @@
 package net.sourceforge.urin;
 
 public final class Scheme {
+
+    private final String value;
+
     public Scheme(final String name) {
+        value = name.toLowerCase();
+    }
+
+    public String value() {
+        return value;
     }
 }
--- src/test/net/sourceforge/urin/SchemeTest.java
+++ src/test/net/sourceforge/urin/SchemeTest.java
@@ -2,9 +2,12 @@ package net.sourceforge.urin;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class SchemeTest {
     @Test
-    public void canCreateAScheme() throws Exception {
-        new Scheme("http");
+    public void schemesAreLowerCased() throws Exception {
+        assertThat(new Scheme("HTTP").value(), equalTo("http"));
     }
 }
