39aac77ab195e844c78601b89671dd8b66992b37
Mark Slater <mark.slater@mail.com>
2011-10-09 18:36:55 +0100

    Mark - First passing integration test.

--- src/test-integration/net/sourceforge/uri/Rfc3986UriExamplesTest.java
+++ src/test-integration/net/sourceforge/uri/Rfc3986UriExamplesTest.java
@@ -1,4 +1,25 @@
+/*
+ * Copyright 2011 Mark Slater
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package net.sourceforge.uri;
+
+import net.sourceforge.urin.Path;
+import net.sourceforge.urin.Scheme;
+import net.sourceforge.urin.Urin;
+import org.junit.Test;
+
+import java.net.URI;
+
+import static net.sourceforge.urin.Urin.urin;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class Rfc3986UriExamplesTest {
 //    ftp://ftp.is.co.za/rfc/rfc1808.txt
@@ -9,4 +30,11 @@ public class Rfc3986UriExamplesTest {
 //    tel:+1-816-555-1212
 //    telnet://192.0.2.16:80/
 //    urn:oasis:names:specification:docbook:dtd:xml:4.1.2
+
+    @Test
+    public void telExample() throws Exception {
+        Urin urin = urin(new Scheme("tel"), new Path("+1-816-555-1212"));
+        assertThat(urin.asString(), equalTo("tel:+1-816-555-1212"));
+        assertThat(urin.asUri(), equalTo(new URI("tel:+1-816-555-1212")));
+    }
 }
