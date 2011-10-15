d2eacf42fe88115a9982723abd3bc943e2312dfe
Mark Slater <mark.slater@mail.com>
2011-10-15 11:37:41 +0100

    Mark - Add more RFC 3986 example tests.

--- src/test-integration/net/sourceforge/uri/Rfc3986UriExamplesTest.java
+++ src/test-integration/net/sourceforge/uri/Rfc3986UriExamplesTest.java
@@ -24,14 +24,49 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class Rfc3986UriExamplesTest {
-//    ftp://ftp.is.co.za/rfc/rfc1808.txt
-//    http://www.ietf.org/rfc/rfc2396.txt
+//     ftp://ftp.is.co.za/rfc/rfc1808.txt
+//     http://www.ietf.org/rfc/rfc2396.txt
 //    ldap://[2001:db8::7]/c=GB?objectClass?one
-//    mailto:John.Doe@example.com
+//     mailto:John.Doe@example.com
 //     news:comp.infosystems.www.servers.unix
 //     tel:+1-816-555-1212
 //     telnet://192.0.2.16:80/
 //     urn:oasis:names:specification:docbook:dtd:xml:4.1.2
+
+    @Test
+    public void ftpExample() throws Exception {
+        Urin urin = urin(
+                new Scheme("ftp"),
+                hierarchicalPart(
+                        authority(registeredName("ftp.is.co.za")),
+                        absolutePath(new Segment("rfc"), new Segment("rfc1808.txt")))
+        );
+        assertThat(urin.asString(), equalTo("ftp://ftp.is.co.za/rfc/rfc1808.txt"));
+        assertThat(urin.asUri(), equalTo(new URI("ftp://ftp.is.co.za/rfc/rfc1808.txt")));
+    }
+
+    @Test
+    public void httpExample() throws Exception {
+        Urin urin = urin(
+                new Scheme("http"),
+                hierarchicalPart(
+                        authority(registeredName("www.ietf.org")),
+                        absolutePath(new Segment("rfc"), new Segment("rfc2396.txt")))
+        );
+        assertThat(urin.asString(), equalTo("http://www.ietf.org/rfc/rfc2396.txt"));
+        assertThat(urin.asUri(), equalTo(new URI("http://www.ietf.org/rfc/rfc2396.txt")));
+    }
+
+    @Test
+    public void mailtoExample() throws Exception {
+        Urin urin = urin(
+                new Scheme("mailto"),
+                hierarchicalPart(
+                        new PathRootlessAbsoluteOrEmpty(new NonEmptySegment("John.Doe@example.com")))
+        );
+        assertThat(urin.asString(), equalTo("mailto:John.Doe@example.com"));
+        assertThat(urin.asUri(), equalTo(new URI("mailto:John.Doe@example.com")));
+    }
 
     @Test
     public void newsExample() throws Exception {
