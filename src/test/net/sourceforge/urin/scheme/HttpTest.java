a3659a58d86ebff4e5eb070439de526fa7d1257e
Mark Slater <mark.slater@mail.com>
2012-07-20 20:05:14 +0100

    Mark - add support for name only HTTP query parameters.

--- src/test/net/sourceforge/urin/scheme/HttpTest.java
+++ src/test/net/sourceforge/urin/scheme/HttpTest.java
@@ -202,18 +202,23 @@ public class HttpTest {
     }
 
     @Test
-    public void multipleQueryParametersCorrectlyConvertsToQuery() throws Exception {
-        assertThat(queryParameters(queryParameter(".+.&.;.=. .", ".+.&.;.=. ."), queryParameter(".+.&.;.=. .", ".+.&.;.=. .")), convertsToQueryString(equalTo(".%2B.%26.%3B.%3D.+.=.%2B.%26.%3B.%3D.+.&.%2B.%26.%3B.%3D.+.=.%2B.%26.%3B.%3D.+.")));
-    }
-
-    @Test
     public void singleQueryParameterCorrectlyConvertsToQuery() throws Exception {
         assertThat(queryParameters(queryParameter(".+.&.;.=. .", ".+.&.;.=. .")), convertsToQueryString(equalTo(".%2B.%26.%3B.%3D.+.=.%2B.%26.%3B.%3D.+.")));
     }
 
     @Test
+    public void multipleQueryParametersCorrectlyConvertsToQuery() throws Exception {
+        assertThat(queryParameters(queryParameter(".+.&.;.=. .", ".+.&.;.=. ."), queryParameter(".+.&.;.=. .", ".+.&.;.=. .")), convertsToQueryString(equalTo(".%2B.%26.%3B.%3D.+.=.%2B.%26.%3B.%3D.+.&.%2B.%26.%3B.%3D.+.=.%2B.%26.%3B.%3D.+.")));
+    }
+
+    @Test
     public void singleValuelessQueryParameterCorrectlyConvertsToQuery() throws Exception {
         assertThat(queryParameters(queryParameter(".+.&.;.=. .")), convertsToQueryString(equalTo(".%2B.%26.%3B.%3D.+.")));
+    }
+
+    @Test
+    public void multiplyValuelessQueryParameterCorrectlyConvertsToQuery() throws Exception {
+        assertThat(queryParameters(queryParameter(".+.&.;.=. ."), queryParameter(".+.&.;.=. .")), convertsToQueryString(equalTo(".%2B.%26.%3B.%3D.+.&.%2B.%26.%3B.%3D.+.")));
     }
 
     @Test
