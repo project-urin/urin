5b37a7216f4772df12ef5ffe4065f4e1a73461e4
Mark Slater <mark.slater@mail.com>
2020-04-16 22:17:33 +0100

    Driveby.

--- src/main/java/net/sourceforge/urin/Authority.java
+++ src/main/java/net/sourceforge/urin/Authority.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Mark Slater
+ * Copyright 2020 Mark Slater
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -24,7 +24,7 @@ import static java.util.Objects.requireNonNull;
  */
 public abstract class Authority {
 
-    private static final Pattern AUTHORITY_PATTERN = Pattern.compile("^((.*)@)?(\\[.*\\]|[^:]*)?(:(.*))?");
+    private static final Pattern AUTHORITY_PATTERN = Pattern.compile("^((.*)@)?(\\[.*]|[^:]*)?(:(.*))?");
 
     private Authority() {
     }
