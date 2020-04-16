4363ec6a48031f48b599dca28f0c186894ee418c
Mark Slater <mark.slater@mail.com>
2020-04-16 22:24:32 +0100

    Driveby.

--- src/main/java/net/sourceforge/urin/UnaryValue.java
+++ src/main/java/net/sourceforge/urin/UnaryValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Mark Slater
+ * Copyright 2020 Mark Slater
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -24,7 +24,7 @@ abstract class UnaryValue<T> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        UnaryValue that = (UnaryValue) o;
+        UnaryValue<?> that = (UnaryValue<?>) o;
         return value.equals(that.value);
     }
 
